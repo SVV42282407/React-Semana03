60a5d20d0c3714f1fcb75fee58326a23ff037ba0
Josue Rodriguez <josue.rodriguez@ASIGBIEM.ORG>
2021-10-09 14:20:08 -0500

    Hackaton

--- src/components/MoviesApp.js
+++ src/components/MoviesApp.js
@@ -9,45 +9,49 @@ import News from './news';
 const App = () => {
     return (
         <div className="container">
-            <div className="row">
-                {movies.map(movie => (
-                    <Banner 
-                        movie={movie.movie}
-                        title={movie.title}
-                        sinopsis={movie.sinopsis}
-                        poster={movie.poster}
-                        ranking={movie.ranking}
-                        categories={movie.categories}
-                        video_url={movie.video_url}
-                    />
-                ))}
-            </div>
-            <div className="row">
-                <div className="left-panel">
-                    <h4>Cast</h4>
-                    {cast.map( item => (
-                        <Cast 
-                            movie={item.movie}
-                            actor={item.actor}
-                            character={item.character}
-                            cast_img={item.cast_img}
+            <div className="header">
+                <div className="row">
+                    {movies.map(movie => (
+                        <Banner 
+                            movie={movie.movie}
+                            title={movie.title}
+                            sinopsis={movie.sinopsis}
+                            poster={movie.poster}
+                            ranking={movie.ranking}
+                            categories={movie.categories}
+                            video_url={movie.video_url}
                         />
                     ))}
                 </div>
-                <div className="right-panel">
-                    <h4>In the news</h4>
-                    {news.map( item => (
-                        <News 
-                            movie={item.movie}
-                            source={item.source}
-                            source_icon={item.source_icon}
-                            title={item.title}
-                            content={item.content}
-                            external_url={item.external_url}
-                            img={item.img}
-                            date={item.date}
+            </div>
+            <div className="main">
+                <div className="row">
+                    <div className="left-panel">
+                        <h4>Cast</h4>
+                        {cast.map( item => (
+                            <Cast 
+                                movie={item.movie}
+                                actor={item.actor}
+                                character={item.character}
+                                cast_img={item.cast_img}
                             />
-                    ))}
+                        ))}
+                    </div>
+                    <div className="right-panel">
+                        <h4>In the news</h4>
+                        {news.map( item => (
+                            <News 
+                                movie={item.movie}
+                                source={item.source}
+                                source_icon={item.source_icon}
+                                title={item.title}
+                                content={item.content}
+                                external_url={item.external_url}
+                                img={item.img}
+                                date={item.date}
+                                />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
